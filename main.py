442db3d5735cc868e59536e4bc9e063f941a4502
Dylan Baker <dylanbaker@uvic.ca>
2026-05-07 16:12:27 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import random
+
+pygame.init()
+
+class DrawingInfo:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GRAY = 128, 128, 128
+    BG_COLOR = BLACK
+
+    def __init__(self, width, height, list):
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
+
+        self.set_list(list)
+
+    def set_list(self, list):
+        pass
